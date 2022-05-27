fb65bccee680fcd46627879c058b10a7a1598d56
Maxwell Calkin <maxwellcalkin@gmail.com>
2022-05-27 12:57:49 -0400

    figured it out because i'm a patient, brilliant coder

--- 6kyu/properFractionNumber.js
+++ 6kyu/properFractionNumber.js
@@ -111,25 +111,25 @@ function properFractions(d){
 
 
 
-def euler(n):
-    res = 1.0*n
-    p = 2
-    while p*p <= n:
-        if n%p == 0:
-            while n%p == 0:
-                n = n/p
-            res *= (1.0 - (1.0/p) )
-        p += 1
+// def euler(n):
+//     res = 1.0*n
+//     p = 2
+//     while p*p <= n:
+//         if n%p == 0:
+//             while n%p == 0:
+//                 n = n/p
+//             res *= (1.0 - (1.0/p) )
+//         p += 1
 
-    if n > 1:
-        res *= (1.0 - (1.0/n) )
+//     if n > 1:
+//         res *= (1.0 - (1.0/n) )
 
-    return int(res)
+//     return int(res)
 
-def proper_fractions(d):
-    if d == 1:
-        return 0
-    return euler(d)
+// def proper_fractions(d):
+//     if d == 1:
+//         return 0
+//     return euler(d)
 
 
 
