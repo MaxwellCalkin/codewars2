097d060365eb0a0eb6162ea356fde1386b691993
Maxwell Calkin <maxwellcalkin@gmail.com>
2022-07-30 21:52:52 -0600

    factorial

---
+++ 7kyu/factorial.js
@@ -0,0 +1,12 @@
+// Your task is to write function factorial.
+
+
+function factorial(n){
+    let result = 1
+    while(n > 1){
+      result *= n
+      n--
+    }
+    return result
+    //your code here
+  }
