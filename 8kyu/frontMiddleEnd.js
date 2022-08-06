7f38e8453b5aabe0c8be6ab7a735019086266a50
Maxwell Calkin <maxwellcalkin@gmail.com>
2022-08-06 16:57:55 -0400

    front mid end

---
+++ 8kyu/frontMiddleEnd.js
@@ -0,0 +1,15 @@
+// You're at the zoo... all the meerkats look weird. Something has gone terribly wrong - someone has gone and switched their heads and tails around!
+
+// Save the animals by switching them back. You will be given an array which will have three values (tail, body, head). It is your job to re-arrange the array so that the animal is the right way round (head, body, tail).
+
+// Same goes for all the other arrays/lists that you will get in the tests: you have to change the element positions with the same exact logics
+
+// Simples!
+
+function fixTheMeerkat(arr) {
+    let front = arr[2]
+    let middle = arr[1]
+    let end = arr[0]
+    
+    return [front, middle, end]
+   }
