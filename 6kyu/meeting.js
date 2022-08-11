3848cc67f94309a3ebd571d5d5be9b21d8737415
Maxwell Calkin <maxwellcalkin@gmail.com>
2022-08-11 14:08:58 -0400

    meeting

---
+++ 6kyu/meeting.js
@@ -0,0 +1,30 @@
+// John has invited some friends. His list is:
+
+// s = "Fred:Corwill;Wilfred:Corwill;Barney:Tornbull;Betty:Tornbull;Bjon:Tornbull;Raphael:Corwill;Alfred:Corwill";
+// Could you make a program that
+
+// makes this string uppercase
+// gives it sorted in alphabetical order by last name.
+// When the last names are the same, sort them by first name. Last name and first name of a guest come in the result between parentheses separated by a comma.
+
+// So the result of function meeting(s) will be:
+
+// "(CORWILL, ALFRED)(CORWILL, FRED)(CORWILL, RAPHAEL)(CORWILL, WILFRED)(TORNBULL, BARNEY)(TORNBULL, BETTY)(TORNBULL, BJON)"
+// It can happen that in two distinct families with the same family name two people have the same first name too.
+
+function meeting(s) {
+    s = s.toUpperCase()
+    s = s.split(';')
+    s = s.map(x => {
+      return x.split(':')
+    })
+    s.forEach(x =>{
+      x.push(x[0])
+      x.shift()
+    })
+    s = s.map(x => {
+      return x.join(', ')
+    })  
+    s.sort()
+    return `(${s.join(')\(')})`
+  }
