38dd1fc6918907ae89686d35f5bb9bbef30f060c
Maxwell Calkin <maxwellcalkin@gmail.com>
2022-08-28 14:04:30 -0400

    counting valleys fixed

--- 7kyu/countingValleys.js
+++ 7kyu/countingValleys.js
@@ -1,3 +1,23 @@
+// You need count how many valleys you will pass.
+
+// Start is always from zero level.
+
+// Every time you go down below 0 level counts as an entry of a valley, and as you go up to 0 level from valley counts as an exit of a valley.
+
+// One passed valley is equal one entry and one exit of a valley.
+
+// s='FUFFDDFDUDFUFUF'
+// U=UP
+// F=FORWARD
+// D=DOWN
+// To represent string above
+
+// (level 1)  __
+// (level 0)_/  \         _(exit we are again on level 0)
+// (entry-1)     \_     _/
+// (level-2)       \/\_/
+// So here we passed one valley
+
 function countingValleys(s) {
     s = s.split('').filter(x => x!=="F")
     
