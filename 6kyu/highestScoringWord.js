490fcdad0db3f91290fd4ab2258d28eec5381ffb
Maxwell Calkin <maxwellcalkin@gmail.com>
2022-08-30 14:41:41 -0400

    highest point word

--- 6kyu/highestScoringWord.js
+++ 6kyu/highestScoringWord.js
@@ -1,3 +1,13 @@
+// Given a string of words, you need to find the highest scoring word.
+
+// Each letter of a word scores points according to its position in the alphabet: a = 1, b = 2, c = 3 etc.
+
+// You need to return the highest scoring word as a string.
+
+// If two words score the same, return the word that appears earliest in the original string.
+
+// All letters will be lowercase and all inputs will be valid.
+
 function high(x){
     const ALPH = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
     let pointsArr = []
