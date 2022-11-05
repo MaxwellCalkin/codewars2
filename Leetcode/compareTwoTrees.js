b47b7fb41be40d97d8e7cce0062cb44a293a287f
Maxwell Calkin <maxwellcalkin@gmail.com>
2022-11-05 16:34:35 -0400

    depth first search to compare two trees in value and structure

---
+++ Leetcode/compareTwoTrees.js
@@ -0,0 +1,20 @@
+//DEPTH FIRST SEARCH PRESERVES SHAPE
+//BREADTH FIRST SEARCH DOES NOT!
+
+function compare(a,b) {
+
+    if (a === null && b === null) {
+        return true
+    }
+
+    if (a === null || b === null) {
+        return false
+    }
+
+    if (a.value !== b.value) {
+        return false
+    }
+
+    return compare(a.left, b.left) && compare(a.right, b.right)
+
+}
